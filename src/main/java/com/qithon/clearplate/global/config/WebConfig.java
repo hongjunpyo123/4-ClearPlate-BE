9a963b829b53bc77334d79eb99c7165a43c84970
june <0721hjp@naver.com>
2025-07-02 23:13:40 +0900

    init: CORS 설정을 위한 WebConfig 설정 클래스 생성

--- src/main/java/com/qithon/clearplate/global/config/WebConfig.java
+++ src/main/java/com/qithon/clearplate/global/config/WebConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * CORS 설정을 위한 클래스입니다.
+ */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
