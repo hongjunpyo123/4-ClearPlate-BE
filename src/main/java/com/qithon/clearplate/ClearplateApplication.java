fb0b1067b48f6ab69ac305b4f82f6c264ed702c9
june <0721hjp@naver.com>
2025-07-02 22:27:42 +0900

    init: 프로젝트 초기세팅 - 이슈, PR 템플릿 생성

---
+++ src/main/java/com/qithon/clearplate/ClearplateApplication.java
@@ -0,0 +1,13 @@
+package com.qithon.clearplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qithon/clearplate/ClearplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qithon.clearplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
