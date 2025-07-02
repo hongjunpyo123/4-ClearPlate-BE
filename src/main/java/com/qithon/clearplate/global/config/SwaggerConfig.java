cf3f12e79cd0976d2f80010350325d73dcb70525
june <0721hjp@naver.com>
2025-07-02 23:18:37 +0900

    init: SwaggerConfig 설정 클래스 생성

---
+++ src/main/java/com/qithon/clearplate/global/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.qithon.clearplate.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI openAPI() {
+    return new OpenAPI()
+        .info(new Info()
+            .title("ClearPlate API")
+            .description("9ithon ClearPlate Project API Documentation")
+            .version("1.0.0"));
+  }
+}
