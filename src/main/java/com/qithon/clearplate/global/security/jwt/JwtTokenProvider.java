aba3a903741ce125b6aedaaae75973e31f56df51
june <0721hjp@naver.com>
2025-07-09 00:54:19 +0900

    feat: JwtTokenProvider 클래스에 jwt 토큰 추출 메서드 추가 - http 요청으로부터 jwt 토큰을 추출하는 메서드를 추가함

--- src/main/java/com/qithon/clearplate/global/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/qithon/clearplate/global/security/jwt/JwtTokenProvider.java
@@ -8,12 +8,14 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.SignatureException;
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.Date;
 import javax.crypto.SecretKey;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 /**
  * JWT 토큰 생성 및 추출, 검증하는 클래스 입니다.
@@ -102,6 +104,27 @@ public class JwtTokenProvider {
       return false;
     }
 
+  }
+
+  /**
+   * 요청 헤더에서 JWT 토큰을 추출하는 메서드입니다.
+   *
+   * @param request HTTP 요청 객체
+   * @return 요청 헤더에서 추출한 순수 JWT 토큰 문자열을 반환합니다
+   */
+  private String getTokenFromRequest(HttpServletRequest request) {
+
+    // Authorization 헤더에서 JWT 토큰을 추출합니다.
+    String token = request.getHeader("Authorization");
+
+    // 토큰이 "Bearer "로 시작하는지 확인합니다.
+    if (StringUtils.hasText(token) && token.startsWith("Bearer ")) {
+
+      //Bearer 제거하고 토큰만을 추출합니다
+      token = token.substring(7);
+    }
+
+    return token;
   }
 
 }
