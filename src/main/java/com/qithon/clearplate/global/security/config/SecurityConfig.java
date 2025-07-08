96d5f141b1bc45e8038fdfb713f6d525579db54d
june <0721hjp@naver.com>
2025-07-08 17:23:46 +0900

    chore: jwt 토큰 필터 추가

--- src/main/java/com/qithon/clearplate/global/security/config/SecurityConfig.java
+++ src/main/java/com/qithon/clearplate/global/security/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.qithon.clearplate.global.security.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qithon.clearplate.global.common.dto.response.ResponseDTO;
+import com.qithon.clearplate.global.exception.ErrorCode;
 import com.qithon.clearplate.global.security.jwt.JwtTokenFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -22,13 +24,49 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
     // 인증 실패 시 반환할 JSON 응답
     String invalidAuthenticationResponse = objectMapper
         .writeValueAsString(ResponseDTO.response(
-            ErrorCode
-        ))
+            ErrorCode.TOKEN_INVALID.getHttpStatus(),
+            ErrorCode.TOKEN_INVALID.getMessage(),
+            null
+        ));
 
+    // 인가 실패 시 반환할 JSON 응답
+    String invalidAuthorizationResponse = objectMapper
+        .writeValueAsString(ResponseDTO.response(
+            ErrorCode.ACCESS_DENIED.getHttpStatus(),
+            ErrorCode.ACCESS_DENIED.getMessage(),
+            null
+        ));
 
+    http
+        .csrf(csrf -> csrf.disable())
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers(
+                "/**"
+            ).permitAll()
+        )
+        .exceptionHandling(e -> e
+
+            //인증 실패 시 응답 핸들링
+            .authenticationEntryPoint(((request, response, authException) -> {
+              response.setStatus(ErrorCode.TOKEN_INVALID.getHttpStatus().value());
+              response.setContentType("application/json");
+              response.getWriter().write(invalidAuthenticationResponse);
+            }))
+
+            //인가 실패 시 응답 핸들링
+            .accessDeniedHandler((request, response, authException) -> {
+              response.setStatus(ErrorCode.TOKEN_INVALID.getHttpStatus().value());
+              response.setContentType("application/json");
+              response.getWriter().write(invalidAuthorizationResponse);
+            }))
+
+        .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
     return http.build();
   }
 
