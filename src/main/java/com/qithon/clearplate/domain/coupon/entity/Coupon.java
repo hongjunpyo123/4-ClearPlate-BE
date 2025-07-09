ca8a8515009b29cde712f4cedd7a3bf6ba58ca0a
june <0721hjp@naver.com>
2025-07-09 21:03:10 +0900

    feat: coupon 엔티티 구현

---
+++ src/main/java/com/qithon/clearplate/domain/coupon/entity/Coupon.java
@@ -0,0 +1,91 @@
+package com.qithon.clearplate.domain.coupon.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Coupon {
+
+  @Id @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, unique = true)
+  private String couponCode;
+
+  @Column(length = 100)
+  private String couponDescription;
+
+  private Integer couponPrice;
+
+  private Integer couponDiscountValue; // 할인 금액
+
+  @CreatedDate
+  private LocalDateTime createdAt;    // 등록일
+
+  private LocalDateTime expiresAt;    // 만료일
+
+  private LocalDateTime usedAt;       // 사용일 (null이면 미사용)
+
+  @Builder
+  private Coupon(String couponCode, String couponDescription, Integer couponPrice
+  , Integer couponDiscountValue, LocalDateTime expiresAt) {
+    this.couponCode = couponCode;
+    this.couponDescription = couponDescription;
+    this.couponPrice = couponPrice;
+    this.couponDiscountValue = couponDiscountValue;
+    this.expiresAt = expiresAt;
+    this.usedAt = null;
+  }
+
+  /**
+   * 쿠폰을 생성하는 정적 팩토리 메서드입니다.
+   * @param couponPrice 쿠폰 가격
+   * @param couponDiscountValue 쿠폰 할인 금액
+   * @param expiresAt 쿠폰 만료일
+   * @param couponDescription 쿠폰 설명
+   * @return 생성된 Coupon 객체
+   */
+  public static Coupon createOf(Integer couponPrice, Integer couponDiscountValue,
+      LocalDateTime expiresAt, String couponDescription) {
+    return Coupon.builder()
+        .couponCode(getCouponCode())
+        .couponDescription(couponDescription)
+        .couponPrice(couponPrice)
+        .couponDiscountValue(couponDiscountValue)
+        .expiresAt(expiresAt)
+        .build();
+  }
+
+
+  /**
+   *  쿠폰 코드를 생성하는 메서드입니다
+   * @return 16자리 랜덤 쿠폰 코드 생성
+   */
+  private static String getCouponCode() {
+      String characters = "0123456789abcdefghijklmnopqrstuvwxyz";
+      SecureRandom random = new SecureRandom();
+      StringBuilder sb = new StringBuilder();
+
+      for (int i = 0; i < 16; i++) {
+        sb.append(characters.charAt(random.nextInt(characters.length())));
+      }
+
+      return sb.toString();
+  }
+
+
+}
