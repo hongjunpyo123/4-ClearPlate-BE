3129152d69f146cad3c8f4427ab9abd2d0e21f6e
june <0721hjp@naver.com>
2025-07-09 21:24:06 +0900

    feat: 쿠폰 등록을 위한 요청 객체 구현

--- src/main/java/com/qithon/clearplate/domain/coupon/controller/CouponController.java
+++ src/main/java/com/qithon/clearplate/domain/coupon/controller/CouponController.java
@@ -1,5 +1,6 @@
 package com.qithon.clearplate.domain.coupon.controller;
 
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/coupon")
 public class CouponController {
+
+  private Integer couponPrice; //쿠폰 가격(교환시 소모되는 포인트임)
+  private Integer couponDiscountValue; //쿠폰 할인 금액
+  private LocalDateTime expiresAt; //쿠폰 만료일 "expiresAt": "2024-12-31T23:59:59"
+  private String couponDescription; //쿠폰 설명
 
 
 }
---
+++ src/main/java/com/qithon/clearplate/domain/coupon/dto/response/RegisterCouponResponse.java
@@ -0,0 +1,10 @@
+package com.qithon.clearplate.domain.coupon.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterCouponResponse {
+
+}
