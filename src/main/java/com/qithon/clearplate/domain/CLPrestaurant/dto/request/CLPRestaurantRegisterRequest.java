db3ad658e01c1bbedd32f13d71ce84557be4fa97
june <0721hjp@naver.com>
2025-07-11 23:59:57 +0900

    chore: 공통응답객체 timestamp 포멧팅

--- src/main/java/com/qithon/clearplate/domain/CLPrestaurant/dto/request/CLPRestaurantRegisterRequest.java
+++ src/main/java/com/qithon/clearplate/domain/CLPrestaurant/dto/request/CLPRestaurantRegisterRequest.java
@@ -1,12 +1,16 @@
 package com.qithon.clearplate.domain.CLPrestaurant.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class CLPRestaurantRegisterRequest {
 
