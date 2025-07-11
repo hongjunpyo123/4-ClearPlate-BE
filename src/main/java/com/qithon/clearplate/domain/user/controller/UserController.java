1eb31bb18d97587c33ec4f01323f7237152e4104
june <0721hjp@naver.com>
2025-07-12 00:26:00 +0900

    docs: 유저 조회 api 설명 추가

--- src/main/java/com/qithon/clearplate/domain/user/controller/UserController.java
+++ src/main/java/com/qithon/clearplate/domain/user/controller/UserController.java
@@ -5,6 +5,10 @@ import com.qithon.clearplate.domain.user.dto.response.UserResponse;
 import com.qithon.clearplate.domain.user.entity.User;
 import com.qithon.clearplate.domain.user.service.UserService;
 import com.qithon.clearplate.global.common.dto.response.ResponseDTO;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,10 +20,50 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
+@Tag(name = "유저 관련 api 입니다.", description = "로그인된 유저 정보를 조회합니다.")
 public class UserController {
 
   private final UserService userService;
 
+
+  @ApiResponse(
+      responseCode = "200",
+      description = "### ✅ 로그인된 유저가 존재할 경우",
+      content = @Content(
+          mediaType = "application/json",
+          schema = @Schema(
+              example = """
+                         {
+                    "timestamp": "2025-07-12 00:24:39",
+                    "data": {
+                      "id": 1,
+                      "socialEmail": "4321933402@kakao",
+                      "nickname": "홍준표",
+                      "cpPoint": 0
+                    }
+                  }
+                        """
+
+          )
+      )
+  )
+  @ApiResponse(
+      responseCode = "400",
+      description = "--- \n### ❌ 로그인된 유저가 없을경우 ",
+      content = @Content(
+          mediaType = "application/json",
+          schema = @Schema(
+              example = """
+                  {
+                     "timestamp": "2025-07-12 00:21:25",
+                     "status": "BAD_REQUEST",
+                     "message": "로그인된 유저가 없습니다."
+                   }
+                 """
+
+          )
+      )
+  )
   @GetMapping
   public ResponseEntity<?> getLoginUser(HttpSession httpSession) {
     try {
