372ddbaa1535211281010405146885a963126c6d
june <0721hjp@naver.com>
2025-07-13 00:25:22 +0900

    chore: point패키지 생성

---
+++ src/main/java/com/qithon/clearplate/domain/point/service/PointService.java
@@ -0,0 +1,10 @@
+package com.qithon.clearplate.domain.point.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PointService {
+
+}
